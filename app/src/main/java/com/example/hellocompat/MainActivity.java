65f45bb2f144c3ae5254527e5596ab607d186795
Patrick Granberg <patgra91@gmail.com>
2020-10-07 21:55:14 +0200

    Save and load state

--- app/src/main/java/com/example/hellocompat/MainActivity.java
+++ app/src/main/java/com/example/hellocompat/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.hellocompat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHelloTextView = findViewById(R.id.hello_textview);
+
+        if (savedInstanceState != null) {
+            mHelloTextView.setText(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
     }
 }
